b2b7753d57bab674d196ea43565befa815a36e46
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-11-05 09:53:20 +0300

    feat: add spatie media library

--- src/Models/Modification.php
+++ src/Models/Modification.php
@@ -3,9 +3,13 @@
 namespace Approval\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Modification extends Model
+class Modification extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
     /**
      * The attributes that can't be filled.
      *
--- src/Models/ModificationRelation.php
+++ src/Models/ModificationRelation.php
@@ -3,9 +3,13 @@
 namespace Approval\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class ModificationRelation extends Model
+class ModificationRelation extends Model implements HasMedia
 {
+    use InteractsWithMedia;
+
     /**
      * The attributes that can't be filled.
      *
