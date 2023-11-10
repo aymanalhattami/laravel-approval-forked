f7787a0f98b9beb083c6ff26954d48177c149856
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-11-10 16:03:23 +0300

    feat: add label function for action enum

--- src/Enums/ActionEnum.php
+++ src/Enums/ActionEnum.php
@@ -11,4 +11,14 @@ enum ActionEnum: string
     case UpdateOrCreate = 'update_or_create';
     case DeleteThenCreate = 'delete_then_create';
 
+    public function label(): string
+    {
+        return match ($this) {
+            self::Create => 'Create',
+            self::Update => 'Update',
+            self::Delete => 'Delete',
+            self::UpdateOrCreate => 'Update or create',
+            self::DeleteThenCreate => 'Delete then create',
+        };
+    }
 }
