14b47424f6810c4d1763c75890139cbda953e278
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-11-10 15:29:59 +0300

    refactor: action enum

--- src/Enums/ActionEnum.php
+++ src/Enums/ActionEnum.php
@@ -5,14 +5,10 @@ namespace Approval\Enums;
 # TODO:: rename to RelationAction
 enum ActionEnum: string
 {
-    case Create = 'create'; # useful for hasMany or belongsToMany relationship
-    case Update = 'update'; # useful for hasMany or belongsToMany relationship
-    case Delete = 'delete'; # useful for hasMany or belongsToMany relationship
-    case UpdateOrCreate = 'update_or_create'; # useful for hasOne relationship
-    case DeleteThenCreate = 'delete_then_create'; # useful for hasMany or belongsToMany relationship
-
-//    case MorphCreate = 'morph_create';
-//    case MorphUpdateOrCreate = 'morph_update_or_create';
-//    case MorphDeleteThenCreate = 'morph_delete_then_create';
+    case Create = 'create';
+    case Update = 'update';
+    case Delete = 'delete';
+    case UpdateOrCreate = 'update_or_create';
+    case DeleteThenCreate = 'delete_then_create';
 
 }
